b1f194d58a9f2518315096825ba762a2510c036a
nikolabo <nikolabo@live.com>
2024-05-27 16:06:05 -0600

    build echo example in container with -trimpath

--- sample/echo/build_images.sh
+++ sample/echo/build_images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-sudo docker build --tag echo-frontend -f Dockerfile-frontend ..
-sudo docker build --tag echo-server -f Dockerfile-server  ..
+sudo docker build --tag echo-frontend -f Dockerfile-frontend ../..
+sudo docker build --tag echo-server -f Dockerfile-server  ../..
 sudo docker tag echo-frontend nikolabo/echo-frontend-grpc
 sudo docker tag echo-server nikolabo/echo-server-grpc
 sudo docker push nikolabo/echo-frontend-grpc
