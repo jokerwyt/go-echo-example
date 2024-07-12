5ae9eb761d654bd5463a17dbedfeebd883b5a70f
Yuyao Wang <yuyao6@cs.washington.edu>
2024-07-12 13:34:25 -0400

    fix: logic of updating to latest version

--- interceptor/plugininterceptor.go
+++ interceptor/plugininterceptor.go
@@ -111,20 +111,20 @@ func updateVersionNumberFromFile(filePath string) {
 }
 
 func updateChains(prefix string) {
-	var highestSeen string
+	var highestSeen string = highestFile
 
 	dir, prefix := filepath.Split(prefix)
 	files, _ := os.ReadDir(dir)
 
 	for _, file := range files {
 		if strings.HasPrefix(file.Name(), prefix) {
-			if file.Name() > highestFile {
+			if file.Name() > highestSeen {
 				highestSeen = file.Name()
 			}
 		}
 	}
 
-	if highestSeen != "" && highestSeen != highestFile {
+	if highestSeen != highestFile {
 		highestFile = highestSeen
 		intercept := loadInterceptors(dir + highestFile)
 		if pluginInterface != nil {
